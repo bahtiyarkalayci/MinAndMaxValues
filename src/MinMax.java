a3f786923ae2a7b7a70a3273d2e70eb3962cdefd
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-15 11:15:04 +0300

    https://www.patika.dev/tr

--- src/MinMax.java
+++ src/MinMax.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class MinMax {
-}
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        int maxNumber=Integer.MIN_VALUE;
+        int minNumber=Integer.MAX_VALUE;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("how many number you will be enter");
+        int number=scanner.nextInt();
+
+        for (int i=1;i<=number;i++){
+            System.out.println("enter"+i+".value");
+            int value =scanner.nextInt();
+            if (value>maxNumber){
+                maxNumber=value;
+
+
+            } else if (value<minNumber) {
+                minNumber=value;
+            }
+
+        }
+        System.out.println("max number ="+maxNumber);
+        System.out.println("min number="+minNumber);
+
+
+
+       }
+    }
+
